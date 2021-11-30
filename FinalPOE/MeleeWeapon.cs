45d467625469941e73fa6bcf85a63fb647cad4d7
Nala Mthembu <Nala00.12@gmail.com>
2021-12-01 00:56:35 +0200

    Many new additions and Changes
    Caught file not found exception when  trying to load a save game file that hasn't been created yet.
    
    -Added a weapon class and a Melee Weapon class

---
+++ FinalPOE/MeleeWeapon.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalPOE
+{
+    public enum Types
+    {
+        DAGGER, LONGSWORD
+    }
+
+    public class MeleeWeapon : Weapon
+    {
+        public new int Range { get { return 1; } } //overridden accessor.
+
+        //✧ <-- melee symbol.
+
+        public MeleeWeapon(Types meleeType, int x = 0, int y = 0) : base('✧', x, y)
+        {
+            switch(meleeType)
+            {
+                case Types.DAGGER:
+                    weaponType = "Dagger";
+                    durability = 10;
+                    damage = 3;
+                    cost = 3;
+                    break;
+
+                case Types.LONGSWORD:
+                    weaponType = "Longsword";
+                    durability = 6;
+                    damage = 4;
+                    cost = 5;
+                    break;
+            }
+        }
+    }
+}
---
+++ FinalPOE/Weapon.cs
@@ -0,0 +1,20 @@
+﻿namespace FinalPOE
+{
+    public abstract class Weapon : Item
+    {
+        protected int damage;
+        protected int range;
+        protected int durability;
+        protected int cost;
+        protected string weaponType;
+
+        public int Damage { get { return damage; } }
+        public int Range { get { return range; } }
+        public int Durability { get { return durability; } }
+        public int Cost { get { return cost; } }
+
+        public string WeaponType { get { return weaponType; } set { weaponType = value; } }
+
+        public Weapon (char symbol, int x = 0, int y = 0) : base (x, y, TileEnum.WEAPON) { }
+    }
+}
